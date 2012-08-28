9047a541d4508a9413fdea8e4d4fe1eca0c7e51b
perchouli <jp.chenyang@gmail.com>
2012-08-28 18:40:34 +0800

    fix

--- ixedit.js
+++ ixedit.js
@@ -17,7 +17,7 @@
 // ixedit object.
 var ixedit = {
 	appName: 'IxEdit',
-	version: '1.01Alpha',
+	version: '1.02Beta',
 	term: '2012',
 	hasLocalDb: false,
 	detectBrowserLang: function(){
@@ -3900,8 +3900,8 @@ ixedit.getIxFromInputs = function(){
 				var commandString = jQuery('.unchian-unknown-command', jQuery(this)).val();
 				var attributesString = jQuery('.unchian-unknown-params', jQuery(this)).val();
 				var attributesSubString = jQuery('.unchian-unknown-subparams', jQuery(this)).val();
-				var commandAttributesValues = ixedit.JSON.parse(unescape(attributesString));
-				var commandAttributesSubValues = ixedit.JSON.parse(unescape(attributesSubString));
+				var commandAttributesValues = JSON.parse(unescape(attributesString));
+				var commandAttributesSubValues = JSON.parse(unescape(attributesSubString));
 
 			}else{ // Normal operation.
 
@@ -3957,7 +3957,7 @@ ixedit.getIxFromInputs = function(){
 				var targetString = jQuery('.unchian-unknown-condtarget', jQuery(this)).val();
 				var commandString = jQuery('.unchian-unknown-condcommand', jQuery(this)).val();
 				var attributesString = jQuery('.unchian-unknown-condparams', jQuery(this)).val();
-				var commandAttributesValues = ixedit.JSON.parse(unescape(attributesString));
+				var commandAttributesValues = JSON.parse(unescape(attributesString));
 
 			}else{
 
@@ -4696,8 +4696,8 @@ ixedit.refreshIxInputBox = function(ixObj){
 		if(ixedit.cmds[theActions.command]){ // If the command is supported.
 			this.appendActionInputs(jQuery('#ixedit-actionitems'), i, theActions.target, theActions.delay, theActions.command, theActions.params, theActions.subparams, false);
 		}else{ // If not.
-			var theParams = escape(ixedit.JSON.stringify(theActions.params)); // Convert params to JSON.
-			var theSubParams = escape(ixedit.JSON.stringify(theActions.subparams)); // Convert subparams to JSON.
+			var theParams = escape(JSON.stringify(theActions.params)); // Convert params to JSON.
+			var theSubParams = escape(JSON.stringify(theActions.subparams)); // Convert subparams to JSON.
 			ixedit.appendUnknownActionInputs(jQuery('#ixedit-actionitems'), i, theActions.target, theActions.delay, theActions.command, theParams, theSubParams, false);
 		};
 	};
@@ -4719,7 +4719,7 @@ ixedit.refreshIxInputBox = function(ixObj){
 			if(ixedit.condcmds[theConditions.command]){ // If the condition is supported.
 				this.appendConditionInputs(jQuery('#ixedit-conditionitems'), i, theConditions.type, theConditions.target, theConditions.command, theConditions.params, false);
 			}else{ // If not.
-				var theParams = escape(ixedit.JSON.stringify(theConditions.params)); // Convert params to JSON.
+				var theParams = escape(JSON.stringify(theConditions.params)); // Convert params to JSON.
 				ixedit.appendUnknownConditionInputs(jQuery('#ixedit-conditionitems'), i, theConditions.type, theActions.target, theConditions.command, theParams, false);
 			};
 
@@ -5159,10 +5159,8 @@ ixedit.xRay.startXRay = function(targetSelectorBox){
 
 // The Time Machine!
 ixedit.preBufferOriginal = function(){
-
 	this.lostWorld = jQuery('<div>' + jQuery('body').html() + '</div>');
 	//this.lostWorld = jQuery('<rosetta>' + jQuery('body').html() + '</rosetta>');
-
 };
 
 
@@ -5643,315 +5641,6 @@ Array.prototype.ixeditSortByNumber = function() {
 		return (parseInt(a) > parseInt(b)) ? 1 : -1;
 	});
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ********** JSON PARSER AND STRINGIFIER **********//
-
-/*!
-    json2.js
-    2008-01-17
-
-    Public Domain
-
-    No warranty expressed or implied. Use at your own risk.
-
-    See http://www.JSON.org/js.html
-
-    This file creates a global JSON object containing two methods:
-
-        JSON.stringify(value, whitelist)
-            value       any JavaScript value, usually an object or array.
-
-            whitelist   an optional array prameter that determines how object
-                        values are stringified.
-
-            This method produces a JSON text from a JavaScript value.
-            There are three possible ways to stringify an object, depending
-            on the optional whitelist parameter.
-
-            If an object has a toJSON method, then the toJSON() method will be
-            called. The value returned from the toJSON method will be
-            stringified.
-
-            Otherwise, if the optional whitelist parameter is an array, then
-            the elements of the array will be used to select members of the
-            object for stringification.
-
-            Otherwise, if there is no whitelist parameter, then all of the
-            members of the object will be stringified.
-
-            Values that do not have JSON representaions, such as undefined or
-            functions, will not be serialized. Such values in objects will be
-            dropped; in arrays will be replaced with null.
-            JSON.stringify(undefined) returns undefined. Dates will be
-            stringified as quoted ISO dates.
-
-            Example:
-
-            var text = JSON.stringify(['e', {pluribus: 'unum'}]);
-            // text is '["e",{"pluribus":"unum"}]'
-
-        JSON.parse(text, filter)
-            This method parses a JSON text to produce an object or
-            array. It can throw a SyntaxError exception.
-
-            The optional filter parameter is a function that can filter and
-            transform the results. It receives each of the keys and values, and
-            its return value is used instead of the original value. If it
-            returns what it received, then structure is not modified. If it
-            returns undefined then the member is deleted.
-
-            Example:
-
-            // Parse the text. If a key contains the string 'date' then
-            // convert the value to a date.
-
-            myData = JSON.parse(text, function (key, value) {
-                return key.indexOf('date') >= 0 ? new Date(value) : value;
-            });
-
-    This is a reference implementation. You are free to copy, modify, or
-    redistribute.
-
-    Use your own copy. It is extremely unwise to load third party
-    code into your pages.
-*/
-
-/*jslint evil: true */
-
-/*global JSON */
-
-/*members "\b", "\t", "\n", "\f", "\r", "\"", JSON, "\\", apply,
-    charCodeAt, floor, getUTCDate, getUTCFullYear, getUTCHours,
-    getUTCMinutes, getUTCMonth, getUTCSeconds, hasOwnProperty, join, length,
-    parse, propertyIsEnumerable, prototype, push, replace, stringify, test,
-    toJSON, toString
-*/
-
-//if (!this.JSON) { // <<----- ORIGINAL
-//
-//    JSON = function () { // <<----- ORIGINAL
-
-if (!ixedit.JSON) { // <<----- MODIFIED
-
-	ixedit.JSON = new function(){ // <<----- MODIFIED
-
-        function f(n) {    // Format integers to have at least two digits.
-            return n < 10 ? '0' + n : n;
-        }; // <<-- ADDED COMMA
-
-        Date.prototype.toJSON = function () {
-
-// Eventually, this method will be based on the date.toISOString method.
-
-            return this.getUTCFullYear()   + '-' +
-                 f(this.getUTCMonth() + 1) + '-' +
-                 f(this.getUTCDate())      + 'T' +
-                 f(this.getUTCHours())     + ':' +
-                 f(this.getUTCMinutes())   + ':' +
-                 f(this.getUTCSeconds())   + 'Z';
-        };
-
-
-        var m = {    // table of character substitutions
-            '\b': '\\b',
-            '\t': '\\t',
-            '\n': '\\n',
-            '\f': '\\f',
-            '\r': '\\r',
-            '"' : '\\"',
-            '\\': '\\\\'
-        };
-
-        function stringify(value, whitelist) {
-            var a,          // The array holding the partial texts.
-                i,          // The loop counter.
-                k,          // The member key.
-                l,          // Length.
-                r = /["\\\x00-\x1f\x7f-\x9f]/g,
-                v;          // The member value.
-
-            switch (typeof value) {
-            case 'string':
-
-// If the string contains no control characters, no quote characters, and no
-// backslash characters, then we can safely slap some quotes around it.
-// Otherwise we must also replace the offending characters with safe sequences.
-
-                return r.test(value) ?
-                    '"' + value.replace(r, function (a) {
-                        var c = m[a];
-                        if (c) {
-                            return c;
-                        }; // <<-- ADDED COMMA
-                        c = a.charCodeAt();
-                        return '\\u00' + Math.floor(c / 16).toString(16) +
-                                                   (c % 16).toString(16);
-                    }) + '"' :
-                    '"' + value + '"';
-
-            case 'number':
-
-// JSON numbers must be finite. Encode non-finite numbers as null.
-
-                return isFinite(value) ? String(value) : 'null';
-
-            case 'boolean':
-            case 'null':
-                return String(value);
-
-            case 'object':
-
-// Due to a specification blunder in ECMAScript,
-// typeof null is 'object', so watch out for that case.
-
-                if (!value) {
-                    return 'null';
-                }; // <<-- ADDED COMMA
-
-// If the object has a toJSON method, call it, and stringify the result.
-
-                if (typeof value.toJSON === 'function') {
-                    return stringify(value.toJSON());
-                }; // <<-- ADDED COMMA
-                a = [];
-                if (typeof value.length === 'number' &&
-                        !(value.propertyIsEnumerable('length'))) {
-
-// The object is an array. Stringify every element. Use null as a placeholder
-// for non-JSON values.
-
-                    l = value.length;
-                    for (i = 0; i < l; i += 1) {
-                        a.push(stringify(value[i], whitelist) || 'null');
-                    }; // <<-- ADDED COMMA
-
-// Join all of the elements together and wrap them in brackets.
-
-                    return '[' + a.join(',') + ']';
-                }; // <<-- ADDED COMMA
-                if (whitelist) {
-
-// If a whitelist (array of keys) is provided, use it to select the components
-// of the object.
-
-                    l = whitelist.length;
-                    for (i = 0; i < l; i += 1) {
-                        k = whitelist[i];
-                        if (typeof k === 'string') {
-                            v = stringify(value[k], whitelist);
-                            if (v) {
-                                a.push(stringify(k) + ':' + v);
-                            }
-                        }
-                    }
-                } else {
-
-// Otherwise, iterate through all of the keys in the object.
-
-                    for (k in value) {
-                        if (typeof k === 'string') {
-                            v = stringify(value[k], whitelist);
-                            if (v) {
-                                a.push(stringify(k) + ':' + v);
-                            }
-                        }
-                    }
-                }; // <<-- ADDED COMMA
-
-// Join all of the member texts together and wrap them in braces.
-
-                return '{' + a.join(',') + '}';
-            }
-        }
-
-        return {
-            stringify: stringify,
-            parse: function (text, filter) {
-                var j;
-
-                function walk(k, v) {
-                    var i, n;
-                    if (v && typeof v === 'object') {
-                        for (i in v) {
-                            if (Object.prototype.hasOwnProperty.apply(v, [i])) {
-                                n = walk(i, v[i]);
-                                if (n !== undefined) {
-                                    v[i] = n;
-                                }
-                            }
-                        }
-                    }; // <<-- ADDED COMMA
-                    return filter(k, v);
-                }; // <<-- ADDED COMMA
-
-
-// Parsing happens in three stages. In the first stage, we run the text against
-// regular expressions that look for non-JSON patterns. We are especially
-// concerned with '()' and 'new' because they can cause invocation, and '='
-// because it can cause mutation. But just to be safe, we want to reject all
-// unexpected forms.
-
-// We split the first stage into 4 regexp operations in order to work around
-// crippling inefficiencies in IE's and Safari's regexp engines. First we
-// replace all backslash pairs with '@' (a non-JSON character). Second, we
-// replace all simple value tokens with ']' characters. Third, we delete all
-// open brackets that follow a colon or comma or that begin the text. Finally,
-// we look to see that the remaining characters are only whitespace or ']' or
-// ',' or ':' or '{' or '}'. If that is so, then the text is safe for eval.
-
-                if (/^[\],:{}\s]*$/.test(text.replace(/\\./g, '@').
-replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
-replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
-
-// In the second stage we use the eval function to compile the text into a
-// JavaScript structure. The '{' operator is subject to a syntactic ambiguity
-// in JavaScript: it can begin a block or an object literal. We wrap the text
-// in parens to eliminate the ambiguity.
-
-                    j = eval('(' + text + ')');
-
-// In the optional third stage, we recursively walk the new structure, passing
-// each name/value pair to a filter function for possible transformation.
-
-                    return typeof filter === 'function' ? walk('', j) : j;
-                }; // <<-- ADDED COMMA
-
-// If the text is not JSON parseable, then a SyntaxError is thrown.
-
-                //throw new SyntaxError('formOutput'); // <<----- ORIGINAL
-
-                window.alert('IxEdit Error: Parsing Data Failed.'); // <<----- MODIFIED
-                throw new SyntaxError('Parsing Data Failed.'); // <<----- MODIFIED
-
-            }
-        };
-    }();
-}
-
-
-
-
-
-
 
 
 
